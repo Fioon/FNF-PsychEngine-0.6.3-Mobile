cb5335afbaacb5b19efbb821ce644a995ed8d0db
Fioon <135500399+Fioon@users.noreply.github.com>
2025-05-04 20:55:19 +0800

    Delete example_mods/data/bummed-old directory

--- example_mods/data/bummed-old/camZoomChange.lua
+++
@@ -1,7 +0,0 @@
-function onUpdate()
-	if mustHitSection == false then
-		setProperty('defaultCamZoom', 0.7)
-	else
-		setProperty('defaultCamZoom', 0.7)
-	end
-end
--- example_mods/data/bummed-old/gameOverSMR.lua
+++
@@ -1,4 +0,0 @@
-function onGameOverStart()
-        startVideo('Game Over/SMR-1')
-	playSound('HomicideGameOverPixel1-VID', 1)
-end
